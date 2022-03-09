453e35e250f9a06ee85c6320fdbb337a1e5c379a
Henderson da Rocha Porfirio <hendersonporfirio@protonmail.com>
2022-03-09 17:59:06 -0300

    # commit 1 - Decorator

---
+++ src/tuyo/designpatterns/App.java
@@ -0,0 +1,23 @@
+package tuyo.designpatterns;
+
+import java.awt.Color;
+
+import tuyo.designpatterns.gui.Window;
+import tuyo.designpatterns.shape.ColorShape;
+import tuyo.designpatterns.shape.DashLineShape;
+import tuyo.designpatterns.shape.Shape;
+import tuyo.designpatterns.shape.Square;
+
+
+public class App {
+
+	public static void main(String[] args) {
+
+		Window w = new Window("Shapes", 300, 300);
+		w.show();
+		
+		Shape shape = new DashLineShape(new ColorShape(new Square(), Color.YELLOW));
+		
+		w.drawShape(shape);
+	}
+}
---
+++ src/tuyo/designpatterns/shape/Shape.java
@@ -0,0 +1,8 @@
+package tuyo.designpatterns.shape;
+
+import java.awt.Graphics;
+
+public interface Shape {
+
+	public void draw(Graphics g);
+}
