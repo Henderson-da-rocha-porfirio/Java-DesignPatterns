217d975ab271f65c5ce581aae4d7bb0d52889318
Henderson da Rocha Porfirio <hendersonporfirio@protonmail.com>
2022-03-10 22:24:56 -0300

    # Commit 4 - Stragegy

--- src/tuyo/designpatterns/Person.java
+++ src/tuyo/designpatterns/Person.java
@@ -1,16 +1,16 @@
 package tuyo.designpatterns;
 
-public class Person {
+public class Person { //Classe que representa uma pessoa que precisa de movimentar: correndo ou andando num trajeto.
 
-	private String name;
+	private String name; //Atributo name: ela terá um nome.
 	
 	public Person(String name) {
 		this.name = name;
-	}
+	} // Construtor com parâmetro name que é copiado para o atributo name.
 	
-	public void move(Movement movement, int distance) {
-		System.out.println(name + " iniciou o movimento");
-		movement.move(distance);
-		System.out.println(name + " terminou o trajeto");
+	public void move(Movement movement, int distance) { // Método movel. Estou afirmando que ela vai executar um movimento. E não está em questão o objeto na memória que vai executar a movimentação: andar ou correr.
+		System.out.println(name + " iniciou o movimento"); // Mostrar o nome da pessoa e que ela iniciou o movimento.
+		movement.move(distance); // Para ela se movimentar basta apenas chamar o método move e passar a distância como parâmetro.
+		System.out.println(name + " terminou o trajeto"); // Aqui fala que a pessoa terminou o trajeto.
 	}
 }
--- src/tuyo/designpatterns/WalkMovement.java
+++ src/tuyo/designpatterns/WalkMovement.java
@@ -1,20 +1,20 @@
 package tuyo.designpatterns;
 
-public class WalkMovement implements Movement {
+public class WalkMovement implements Movement { // Classe do "movimento de andar" que implementa a interface Movement.
 
 	@Override
-	public void move(int distance) {
-		int currentDistance = 0;
+	public void move(int distance) { // Sou obrigado a implementar o método move uma vez que estou implementando de Movement.
+		int currentDistance = 0; // Variável que calcula o começo do que ela já andou. OU seja, começa em 0.
 		
-		while (currentDistance < distance) {
-			currentDistance++;
+		while (currentDistance < distance) { // loop.
+			currentDistance++; // incrememnto em 1 a distância da pessoa.
 			
 			try {
-				Thread.sleep(300);
-			} catch (InterruptedException e) {
+				Thread.sleep(300); // Para que o código não fique muito rápido, eu vou chamar o método Thread.sleep para atrasar a execução.
+			} catch (InterruptedException e) { // tratamento da exceção
 			}
 			
-			if (currentDistance % 20 == 0) {
+			if (currentDistance % 20 == 0) { // a cada 20m a pessoa dá uma parada para descansar. Ou seja, divido o currentDistance por 20 e o resto dessa divisão for 0, significa que a minha distância é divisível por 20.
 				try {
 					Thread.sleep(300);
 				} catch (InterruptedException e) {
@@ -23,7 +23,7 @@ public class WalkMovement implements Movement {
 				System.out.println("Parada para descanso");
 			}
 			
-			System.out.println("Dist�ncia percorrida: " + currentDistance + "m");
+			System.out.println("Distância percorrida: " + currentDistance + "m");
 		}
 	}
 }
